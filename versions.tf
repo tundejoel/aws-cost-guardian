dd9a4e507ff69b80e98649f33596a2df04c46dff
Adedayo Afolabi <officialtundejoel@gmail.com>
2026-09-16 02:44:25 -0500

    Add Terraform versions and AWS provider with default tags

---
+++ versions.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project   = "aws-cost-guardian"
+      ManagedBy = "terraform"
+    }
+  }
+}
